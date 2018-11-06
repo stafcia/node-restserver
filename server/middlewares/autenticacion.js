9a5b4e7d8aa334b80a9758a0d52ff65f01878e1e
Gabriel <gaceves@socialideas.mx>
2018-11-05 18:21:05 -0600

    Quite log token

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -7,7 +7,6 @@ const jwt = require('jsonwebtoken');
 
 let verificaToken = (req, res, next) => {
     let token = req.get('token');
-    console.log(token);
     jwt.verify(token, process.env.SEED, (err, decoded) => {
         if (err) {
             return res.status(401).json({
